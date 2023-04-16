5a7e96a378357ffa337350d35318d75f1eac3261
Mahmoud Haney <mahmoudhaney25@gmail.com>
2023-04-16 09:35:09 +0200

    Prepare the environment for the backend system

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("upload"));
+
+// const auth = require('./routes/auth');
+
+app.listen(5000, "localhost", () => {
+    console.log("SERVER IS RUNNING");
+});
+
+
+// app.use("/auth", auth);
